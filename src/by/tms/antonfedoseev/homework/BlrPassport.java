00eb60f4b3a669bf05ee9c28b335326f28722a40
Anton Fedoseev <fedoseev446@gmail.com>
2023-02-27 20:17:00 +0300

    HomeWork15

--- src/by/tms/antonfedoseev/homework/BlrPassport.java
+++ src/by/tms/antonfedoseev/homework/BlrPassport.java
@@ -14,8 +14,8 @@ public class BlrPassport {
     private LocalDate dateOfIssue;
     private LocalDate dateOfExpiry;
 
-    public BlrPassport(String passportNumber, String lastName, String name, String sex, LocalDate dateOfBirth, String identificationNumber, LocalDate dateOfIssue, LocalDate dateOfExpiry) {
-        System.out.println(passportNumber + lastName + name + sex + dateOfBirth + identificationNumber + dateOfIssue + dateOfExpiry);
+    public BlrPassport(String lastName, String name, String sex, LocalDate dateOfBirth, String passportNumber,
+                       String identificationNumber, LocalDate dateOfIssue, LocalDate dateOfExpiry) {
         if (isValidPassport(lastName, name, passportNumber, identificationNumber, dateOfIssue, dateOfExpiry)) {
             this.lastName = lastName;
             this.name = name;
@@ -30,6 +30,12 @@ public class BlrPassport {
         }
     }
 
+    public boolean isValidPassport(String lastName, String name, String passportNumber,
+                                   String identificationNumber, LocalDate dateOfIssue, LocalDate dateOfExpiry) {
+        return isValidNameAndLastName(name, lastName) && isValidPassNumber(passportNumber) && isValidDataUse(dateOfIssue, dateOfExpiry) && isValidIDNumber(identificationNumber);
+
+    }
+
     public boolean isValidPassNumber(String passportNumber) {
         if (passportNumber.length() != LENGTH_PASSPORT_NUMBER) {
             throw new IllegalArgumentException("Invalid passport number");
@@ -41,47 +47,31 @@ public class BlrPassport {
         if (identificationNumber.length() != LENGTH_ID_NUMBER_BLR) {
             throw new IllegalArgumentException("Invalid ID number");
         }
-        return identificationNumber.matches("\\d{7}[ABCKEMH]\\d{3}[PB|BA|BI]\\d");
+        return identificationNumber.matches("\\d{7}[ABCKEMH]\\d{3}(PB|BA|BI)\\d");
     }
-
-
     public boolean isValidNameAndLastName (String name, String lastName) {
         if (name == null || lastName == null) {
             throw new IllegalArgumentException("Invalid name or lastname");
         }
         return name.matches("^[a-zA-Z]+$") && lastName.matches("^[a-zA-Z]+$");
     }
-
     public boolean isValidDataUse (LocalDate dateOfIssue, LocalDate dateOfExpiry) {
         return  (dateOfIssue.isBefore(dateOfExpiry));
     }
     public boolean isExpired (LocalDate now) {
     return now.isBefore(dateOfExpiry);
     }
-
-    public boolean isValidPassport (String lastName, String name, String passportNumber,
-                                    String identificationNumber, LocalDate dateOfIssue, LocalDate dateOfExpiry) {
-        return isValidNameAndLastName(name, lastName) && isValidPassNumber(passportNumber) &&
-                isValidIDNumber(identificationNumber) && isValidDataUse(dateOfIssue, dateOfExpiry);
-    }
-
     public String toString () {
-        return  "Belarusian Passport: \n"+
-                "Last name - " + lastName + "\n"+
+        return "Belarusian Passport: \n" +
+                "Last name - " + lastName + "\n" +
                 "Name - " + name + "\n" +
                 "Sex - " + sex + "\n" +
                 "Date of berth - " + dateOfBirth + "\n" +
                 "Passport number - " + passportNumber + "\n" +
                 "Identification number - " + identificationNumber + "\n" +
                 "Date of issue - " + dateOfIssue + "\n" +
-                "Date of expiry - " + dateOfExpiry + "\n";
+                "Date of expiry - " + dateOfExpiry;
     }
-
-//    LocalDate passportExpirationDate = LocalDate.of(2025, 2, 13);
-//    boolean expired = nowDate.isAfter(passportExpirationDate);
-//System.out.println(expired);
-
-
 }
 
 
--- src/by/tms/antonfedoseev/homework/homework15.java
+++ src/by/tms/antonfedoseev/homework/homework15.java
@@ -9,40 +9,28 @@ public class homework15 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter passport number: ");          //!!!!!!!!!!!!!!!!!!!!!
-        String userPassportNumber = scanner.nextLine();
-        System.out.println(userPassportNumber);
-
         System.out.println("Enter your last name: ");
         String userLastName = scanner.nextLine();
-
         System.out.println("Enter yor name: ");
         String userName = scanner.nextLine();
-
         System.out.println("Enter your sex: ");
         String userSex = scanner.nextLine();
-
         System.out.println("Enter your date of berth: ");
         LocalDate userDateOfBerth = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
-
-        System.out.println("Enter identification number: ");      //!!!!!!!!!!!!!!!!!!!!!!!!
+        System.out.println("Enter passport number: ");
+        String userPassportNumber = scanner.nextLine();
+        System.out.println("Enter identification number: ");
         String userIdentificationNumber = scanner.nextLine();
-        System.out.println(userIdentificationNumber);
-
         System.out.println("Enter date of issue: ");
         LocalDate userDateOfIssue = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
         System.out.println("Enter date of expiry: ");
         LocalDate userDateOfExpiry = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
-
-        BlrPassport blrPassport = new BlrPassport(userPassportNumber, userLastName, userName, userSex, userDateOfBerth,
-                userIdentificationNumber, userDateOfIssue, userDateOfExpiry);
+        BlrPassport blrPassport = new BlrPassport(userLastName, userName, userSex, userDateOfBerth,
+                userPassportNumber, userIdentificationNumber, userDateOfIssue, userDateOfExpiry);
 
         System.out.println(blrPassport);
         System.out.println("Passport expiry :" + blrPassport.isExpired(LocalDate.now(ZoneId.of("Europe/Minsk"))));
-
 
 
     }
