539c81081468698d78837442b2e837498abe5309
Anton Fedoseev <fedoseev446@gmail.com>
2023-03-02 14:38:51 +0300

    Corrected HW15

--- src/by/tms/antonfedoseev/homework/TextBlackListFilter.java
+++ src/by/tms/antonfedoseev/homework/TextBlackListFilter.java
@@ -7,13 +7,13 @@ import java.util.regex.Pattern;
 public class TextBlackListFilter {
     private static final String MASK = "####";
     private String[] badWords;
-    private final Pattern[] pattern;
+    private final Pattern[] patterns;
 
     public TextBlackListFilter(String... badWords) {
         this.badWords = Arrays.copyOf(badWords, badWords.length);
-        this.pattern = new Pattern[badWords.length];
+        this.patterns = new Pattern[badWords.length];
         for (int i = 0; i < badWords.length; i++) {
-            pattern[i] = Pattern.compile("\\b" + Pattern.quote(badWords[i]) + "\\b",
+            patterns[i] = Pattern.compile("\\b" + Pattern.quote(badWords[i]) + "\\b",
                     Pattern.UNICODE_CHARACTER_CLASS |
                             Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
         }
@@ -21,7 +21,7 @@ public class TextBlackListFilter {
 
     public boolean hasBadWords(String text) {
         for (int i = 0; i < badWords.length; i++) {
-            Pattern pattern = this.pattern[i];
+            Pattern pattern = this.patterns[i];
             Matcher matcher = pattern.matcher(text);
             if (matcher.find()) {
                 return true;
@@ -32,7 +32,7 @@ public class TextBlackListFilter {
     public int getCountBadWords(String text) {
         int count = 0;
         for (int i = 0; i < badWords.length; i++) {
-            Pattern pattern = this.pattern[i];
+            Pattern pattern = this.patterns[i];
             Matcher matcher = pattern.matcher(text);
             while (matcher.find()) {
                     count++;
@@ -44,7 +44,7 @@ public class TextBlackListFilter {
     public String hideBadWords(String text) {
         String result = text;
         for (int i = 0; i < badWords.length; i++) {
-            Pattern pattern = this.pattern[i];
+            Pattern pattern = this.patterns[i];
             Matcher matcher = pattern.matcher(text);
             result = matcher.replaceAll(MASK);
         }
